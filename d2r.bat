02fe02499e22535431420b05f0e14f4ed5d06521
Portentera <107505708+Portentera@users.noreply.github.com>
2022-12-31 23:57:58 +0800

    Update d2r.bat

--- d2r.bat
+++ d2r.bat
@@ -1,5 +1,5 @@
 set folder="C:\Program Files (x86)\Diablo II Resurrected"
-set handle="C:\handle.exe"
+set handle=C:\handle.exe
 START "" /D %folder% D2R.exe %*
 timeout 5 > NUL
 for /f "tokens=3,6 delims= " %%a in ('%handle% -a "Check For Other Instances" -nobanner') do %handle% -p %%a -c %%b -y
